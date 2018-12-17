6400e19296943c95b1e30e990776c25dd14571e4
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-12-17 21:06:58 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,3 +5,12 @@ def my_find(collection)
 end
 
 
+require 'pry'
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return(collection[i]) if yield(collection[i])
+    i += 1
+  end
+end
